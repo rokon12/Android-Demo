702b515e0eaaab5792cdd0d2ea426750ec7f42bd
anmbrr <anm_brr@yahoo.com>
2011-07-23 21:26:59 +0600

    first commit

---
+++ src/org/codexplo/rokonoid/AndroidDemoActivity.java
@@ -0,0 +1,13 @@
+package org.codexplo.rokonoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidDemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
